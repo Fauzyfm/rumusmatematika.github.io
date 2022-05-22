6fc7d6c9bd89f2eddbd009251f9e25180a028eec
fachrulfauzy <fachrulfauzy23@gmail.com>
2022-05-22 19:28:06 +0700

    menambahkan shadow

--- src/halaman/main.js
+++ src/halaman/main.js
@@ -6,8 +6,8 @@ const Main = () => {
         <div className="container mt-5">
             <div className="row ">
                 <div className="col-md-12">
-                    <div className="list-group shadow-lg">
-                    <Link to="/main/rbd" class="list-group-item list-group-item-action msover mb-2 rounded ">
+                    <div className="list-group ">
+                    <Link to="/main/rbd" class="list-group-item list-group-item-action msover mb-2 rounded shadow-lg ">
                         <div class="d-flex w-100 justify-content-between ">
                         <h5 class="mb-1">Rumus Matematika Bangun Datar</h5>
                         <small class="text-muted">...</small>
@@ -15,7 +15,7 @@ const Main = () => {
                         <p class="mb-1">Kalian bisa menghitung rumus pada sebuah Bangun Datar.</p>
                         <small class="text-muted">contoh: persegi panjang, segitiga, lingkaran.</small>
                     </Link>
-                    <Link to="#" class="list-group-item list-group-item-action msover border border-3 border-white mb-2 rounded">
+                    <Link to="#" class="list-group-item list-group-item-action msover mb-2 rounded shadow-lg">
                         <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Coming Soon</h5>
                         <small class="text-muted">...</small>
@@ -23,7 +23,7 @@ const Main = () => {
                         <p class="mb-1">coming soon.</p>
                         <small class="text-muted">coming soon.</small>
                     </Link>
-                    <Link to="#" class="list-group-item list-group-item-action msover border border-3 border-white mb-2 rounded">
+                    <Link to="#" class="list-group-item list-group-item-action msover mb-2 rounded shadow-lg">
                         <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Coming Soon</h5>
                         <small class="text-muted">...</small>
@@ -31,7 +31,7 @@ const Main = () => {
                         <p class="mb-1">coming soon.</p>
                         <small class="text-muted">coming soon.</small>
                     </Link>
-                    <Link to="#" class="list-group-item list-group-item-action msover border border-3 border-white mb-2 rounded">
+                    <Link to="#" class="list-group-item list-group-item-action msover mb-2 rounded shadow-lg">
                         <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Coming Soon</h5>
                         <small class="text-muted">...</small>
@@ -39,7 +39,7 @@ const Main = () => {
                         <p class="mb-1">coming soon.</p>
                         <small class="text-muted">coming soon.</small>
                     </Link>
-                    <Link to="#" class="list-group-item list-group-item-action msover border border-3 border-white rounded">
+                    <Link to="#" class="list-group-item list-group-item-action msovermb-2  rounded shadow-lg">
                         <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Coming Soon</h5>
                         <small class="text-muted">...</small>
